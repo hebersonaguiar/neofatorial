b9725d40a40bf86aa8d002c7449a460f8d889295
Heberson Rocha Aguiar <haguiar.hepta@epl.gov.br>
2020-01-10 16:29:59 -0300

    Alterando flash message

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def index():
 			else:
 			   for i in range(1,num + 1):
 			       factorial = factorial*i
-			       flash('O factorial é ' + num ".", 'info')
+			       flash('O factorial é ' + num + ".", 'info')
 			   print("O factorial do numero ",num," e ",factorial)
 			   # flash('O fatorial de ' + num + ' é '+ factorial, 'info')
 			   # flash('o fatorial de ' + num + ' é ' + factorial + '.', 'info')
