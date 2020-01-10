2ab7fec4e96f53b941eef5b0271bb214ad1ed8f0
Heberson Rocha Aguiar <haguiar.hepta@epl.gov.br>
2020-01-10 16:32:57 -0300

    Alterando flash message

--- app.py
+++ app.py
@@ -25,18 +25,13 @@ def index():
 			factorial = 1
 			# check if the number is negative, positive or zero
 			if num < 0:
-			   print("Desculpe, não podemos calcular factorial de numeros negativos.")
 			   flash('Desculpe, não podemos calcular factorial de numeros negativos.', 'danger')
 			elif num == 0:
-			   print("O factorial de 0 e 1")
 			   flash('O factorial de 0 é 1', 'info')
 			else:
 			   for i in range(1,num + 1):
 			       factorial = factorial*i
-			   print("O factorial do numero ",num," e ",factorial)
-			   flash('O factorial é ' + str(num) + ".", 'info')
-			   # flash('O fatorial de ' + num + ' é '+ factorial, 'info')
-			   # flash('o fatorial de ' + num + ' é ' + factorial + '.', 'info')
+			   flash('o fatorial de ' + str(num) + ' é ' + str(factorial) + '.', 'info')
 
 		except Exception as error_message:
 			return redirect(url_for('index'))
